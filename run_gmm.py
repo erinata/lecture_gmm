0a3cf31480cd62dc27db42662bedfbcb61ccd803
Chungsang Tom Lam <erinata@gmail.com>
2019-04-11 15:57:12 -0400

    try kmeans

--- run_gmm.py
+++ run_gmm.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+from sklearn.cluster import KMeans
+
 dataset = pd.read_csv("dataset.csv"
 					,header=None)
 
@@ -9,4 +11,7 @@ print(dataset.head())
 plt.scatter(dataset[0],dataset[1])
 plt.savefig("scatter.png")
 
+kmeans_predictions = KMeans(n_clusters=3).fit_predict(dataset)
+plt.scatter(dataset[0],dataset[1], c=kmeans_predictions)
+plt.savefig("scatter_kmean3.png")
 
